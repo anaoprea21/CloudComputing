fd18b7c3c8f49d8c2ccffabb467ac6028e23537a
emma-elena <79704499+emma-elena@users.noreply.github.com>
2024-11-30 17:37:50 +0200

    Update models.py

--- models.py
+++ models.py
@@ -4,8 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Food(db.Model):
-    tablename = 'foods'
+    tablename = "foods"
 
     name = db.Column(db.String(25), primary_key=True)
     restaurant = db.Column(db.String(255), nullable=False)
@@ -13,27 +14,25 @@ class Food(db.Model):
     calories = db.Column(db.Integer, nullable=False)
     type = db.Column(db.String(255), nullable=False)
 
-
     def init(self, name, restaurant, price, calories, type):
         self.name = name
-        self.restaurant = autrestauranthor
+        self.restaurant = restaurant
         self.price = price
-	self.calories = calories
-	self.type = type
+        self.calories = calories
+        self.type = type
 
     @classmethod
     def add_food(cls, food_data):
         food = {
-            'name': food_data['name'],
-            'restarant': food_data['restaurat'],
-            'calories': food_data['calories'],
-            'price': food_data['price'],
-	    'type': food_data['type']
-
+            "name": food_data["name"],
+            "restarant": food_data["restaurat"],
+            "calories": food_data["calories"],
+            "price": food_data["price"],
+            "type": food_data["type"],
         }
         send_message_to_queue(food)
 
-        return food['name']
+        return food["name"]
 
     @classmethod
     def get_food(cls, name):
@@ -47,14 +46,15 @@ class Food(db.Model):
 
     def to_dict(self):
         return {
-            'name': self.name,
-            'restaurant': self.restaurant,
-            'price': self.price,
-	    'calories': self.calories,
-	    'type': self.type
+            "name": self.name,
+            "restaurant": self.restaurant,
+            "price": self.price,
+            "calories": self.calories,
+            "type": self.type,
         }
+
 
 def send_message_to_queue(food_data):
     channel = connect_rabbitmq()
-    send_message(channel, 'food_queue', food_data)
+    send_message(channel, "food_queue", food_data)
     channel.close()
